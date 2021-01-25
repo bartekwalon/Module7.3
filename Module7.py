91820e255b4123973a79c411d550e1a0eb45bd22
bartekwalon <75641536+bartekwalon@users.noreply.github.com>
2021-01-25 23:36:19 +0100

    Updated version 25.01

--- Module7.py
+++ Module7.py
@@ -2,24 +2,34 @@ class BaseContact:
     def __init__(self, name, surname, phone, email):
         self.name, self.surname, self.phone, self.email = name, surname, phone, email
 
-@property
-def full_name(self):
-    return f'{self.name} {self.surname}'
+    @property
+    def full_name(self):
+        return f'{self.name} {self.surname}'
 
-def contact(self):
-    print (f'Wybieram numer +48 {self.phone} i dzwonię do {self.full_name}')
+    def contact(self):
+        print (f'Wybieram numer +48 {self.phone} i dzwonię do {self.full_name}')
 
-def __str__(self):
-    return self.full_name
+    def __str__(self):
+        return self.full_name
 
-__repr__ = __str__
+    def label_length(self):
+        print(f'Długość imienia: {len(self.name)} i długość nazwiska {len(self.surname)}') 
+
+    def create_contacts(self):
+        """"""
+        for i in range (10):
+            print (f'{fake.first_name},{fake.last_name}')
+
+    __repr__ = __str__
+
+from faker import Faker
 
 if __name__ == '__main__':
     family = [
         BaseContact('Piotr', 'Kowalski', 123456789, 'piotr.kowalski@wp.pl'),
         BaseContact('Aleksandra', 'Nowak',122334455, 'aleksandra.nowak@wp.pl')
     ]
-
+    fake = Faker
 
 class BusinessContact(BaseContact):
 
@@ -29,17 +39,23 @@ class BusinessContact(BaseContact):
 
     def contact(self):
         super().contact()
-        print(f'Wybieram numer +48 {self.company_phone} i dzwonieę do {self.full_name}')
+        print(f'Wybieram numer +48 {self.company_phone} i dzwonię do {self.full_name}')
+
+    def label_length(self):
+        print(f'Długość imienia: {len(self.name)} i długość nazwiska {len(self.surname)}')
+
 
 if __name__ == '__main__':
     family = [
         BaseContact('Piotr', 'Kowalski', 123456789, 'piotr.kowalski@wp.pl'),
         BaseContact('Aleksandra', 'Nowak',122334455, 'aleksandra.nowak@wp.pl'),
-        BusinessContact('Karol','Zyk',1234556778, 'k.zyk@apple.com','CFO','Apple',133987234)
+        BusinessContact('CFO','Apple',133987234,'Krzysztof','Zyk',1234556778,'k.zyk@apple.com')
     ]
     family[1].surname = family[0].surname
     print(sorted(family, key=lambda contact:contact.surname))
+    fake = Faker   
 
     for person in family:
-        person.contact
+        person.contact()
 
+print(fake.create_contacts())
